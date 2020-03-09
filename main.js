1a72a7f60e279c196e7f5ff808b63a3d1c8fa808
Evgeny Artemov <53362802+evgeny_artemov@users.noreply.github.com>
2020-03-10 00:26:50 +0300

    - Scroll Reveal Animations

--- main.js
+++ main.js
@@ -10,3 +10,31 @@ const menuToggle = () => {
 }
 
 menuToggle();
+
+ScrollReveal().reveal('.animate--left',{
+    distance: '150%',
+    origin: 'left',
+    delay: 200,
+    opacity: null
+})
+
+ScrollReveal().reveal('.animate--right',{
+    distance: '150%',
+    origin: 'right',
+    delay: 200,
+    opacity: null
+})
+
+ScrollReveal().reveal('.animate--top',{
+    distance: '250%',
+    origin: 'top',
+    delay: 0,
+    opacity: 0
+})
+
+ScrollReveal().reveal('.animate--bottom',{
+    distance: '250%',
+    origin: 'bottom',
+    delay: 0,
+    opacity: 0
+})
